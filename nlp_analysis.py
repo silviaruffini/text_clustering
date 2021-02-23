836c7fdd2c72e791f12776c182e51a37cb1cee1b
silviaruffini <ruffinisilvia@gmail.com>
2021-02-23 10:06:52 +0100

    Add files via upload

---
+++ nlp_analysis.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 19:29:27 2021
+
+@author: s.ruffini
+"""
+import pandas as pd
+import preprocessing_text as pt
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+df= pd.read_csv("eu_projects.csv", sep=";")
+
+print(df.head(n=5))
+df_clean = pt.preprocessing_process(df,'description')
+
+
+#define vectorizer parameters
+vectorizer = TfidfVectorizer(ngram_range=(1,1))
+
+# Generate matrix of word vectors
+tfidf_matrix = vectorizer.fit_transform(df_clean['description'])
+
+
